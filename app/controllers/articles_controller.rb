275e9c6efa214afda7d238a112f94c4e8ddd5aec
Jim Weirich <jim.weirich@gmail.com>
2012-03-08 08:37:56 -0800

    Added json formats to article.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,25 +2,43 @@ class ArticlesController < ApplicationController
 
   def index
     @articles = Article.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @articles }
+    end
   end
 
   def show
     @article = Article.find(params[:id])
 #    @comment = @article.comments.new
     @new_comment = Comment.new(article: @article)
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @article }
+    end
   end
 
   def new
     @article = Article.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @article }
+    end
   end
 
   def create
     @article = Article.new(params[:article])
-    if @article.save
-      flash[:notice] = "Created new article '#{@article.title}'"
-      redirect_to article_path(@article)
-    else
-      render :new
+    respond_do do |format|
+      if @article.save
+        format.html { redirect_to article_path(@article), notice: "Created new article '#{@article.title}'" }
+        format.json { render json: @article, status: :created, location: @article }
+      else
+        format.html { render :new }
+        format.json { render json: @article.errors, status: :unprocessable_entity }
+      end
     end
   end
 
@@ -30,18 +48,24 @@ class ArticlesController < ApplicationController
 
   def update
     @article = Article.find(params[:id])
-    if @article.update_attributes(params[:article])
-      flash[:notice] = "Updated article '#{@article.title}'"
-      redirect_to article_path(@article)
-    else
-      render :edit
+    respond_to do |format|
+      if @article.update_attributes(params[:article])
+        format.html { redirect_to article_path(@article), notice: "Updated article '#{@article.title}'" }
+        format.json { head :no_content }
+      else
+        format.html { render :edit }
+        format.json { render json: @article.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
-    redirect_to articles_path, notice: "Deleted article '#{@article.title}'"
+    respond_to do |format|
+      format.html { redirect_to articles_path, notice: "Deleted article '#{@article.title}'" }
+      format.json { head :no_content }
+    end
   end
 
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,14 +2,18 @@ class CommentsController < ApplicationController
 
   def create
     @article = Article.find(params[:article_id])
-#    @new_comment = Comment.create(params[:comment].merge(article: @article))
     @new_comment = @article.comments.build(params[:comment])
-    if @new_comment.save
-      flash[:notice] = "Created new comment for '#{@article.title}'"
-      redirect_to article_path(@article)
-    else
-      @article.reload
-      render "articles/show"
+    respond_to do |format|
+      if @new_comment.save
+        format.html { redirect_to article_path(@article), notice: "Created new comment for '#{@article.title}'" }
+        format.json { render json: @comment, status: :created, location: @comment }
+      else
+        format.html {
+          @article.reload
+          render "articles/show"
+        }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
     end
   end
 
